c5d8dbba4dcc537b45f3edaa0ff0bf31a813c07a
Alasdair <alasdair.armstrong@gmail.com>
2013-10-15 02:39:48 +0100

    Started on S/M proof for inf languages

--- Models/Stutter_Language.thy
+++ Models/Stutter_Language.thy
@@ -107,56 +107,104 @@ lemma stutter_infinite [dest]: "ys \<in> stutter xs \<Longrightarrow> \<not> lfi
 lemma Stutter_l_prod [simp]: "(X \<cdot> Y)\<^sup>\<dagger> = (X\<^sup>\<dagger> \<cdot> Y\<^sup>\<dagger>)\<^sup>\<dagger>"
   sorry
 
-definition ldeleteLeft :: "('a + 'b) llist \<Rightarrow> ('a + 'b) llist" where
-  "ldeleteLeft xs = ltakeWhile is_right xs \<frown> ltl (ldropWhile is_right xs)"
+definition ldeleteLeft :: "enat \<Rightarrow> ('a + 'b) llist \<Rightarrow> ('a + 'b) llist" where
+  "ldeleteLeft n xs = ltakeWhile is_right xs \<frown> ltl (ldropWhile is_right xs)"
 
-lemma "xs' \<in> stutter xs \<Longrightarrow> zs \<in> xs' \<sha> ys \<Longrightarrow>
-  (\<exists>zs'. stutter (lmap \<langle>id,id\<rangle> zs) = stutter zs' \<and> zs' \<in> lmap \<langle>id,id\<rangle> ` (xs \<sha> ys))"
-proof (induct xs' arbitrary: zs rule: stutter.induct)
-  case (self \<sigma>)
-  
-  hence "stutter (lmap \<langle>id,id\<rangle> zs) = stutter (lmap \<langle>id,id\<rangle> (LCons (\<sigma>, \<sigma>) xs \<triangleright> traj zs \<triangleleft> ys))"
-    by (metis (lifting, full_types) mem_Collect_eq reinterleave tshuffle_words_def)
-  also have "... = stutter (lmap \<langle>id,id\<rangle> (xs \<triangleright> ldeleteLeft (traj zs) \<triangleleft> ys))"
-    sorry
-  finally have "stutter (lmap \<langle>id,id\<rangle> zs) = stutter (lmap \<langle>id,id\<rangle> (xs \<triangleright> ldeleteLeft (traj zs) \<triangleleft> ys))" .
+definition linsertLeft :: "enat \<Rightarrow> 'a \<Rightarrow> ('a + 'b) llist \<Rightarrow> ('a + 'b) llist" where
+  "linsertLeft n x xs = undefined"
 
-  moreover have "xs \<triangleright> ldeleteLeft (traj zs) \<triangleleft> ys \<in> xs \<sha> ys"
-    sorry
+definition ltakeLeft :: "nat \<Rightarrow> ('a + 'b) llist \<Rightarrow> ('a + 'b) llist" where
+  "ltakeLeft = undefined"
 
-  ultimately show ?case
-    by auto
-next
-  case (stutter xs1 xs2 \<sigma>)
-  
+definition ldropLeft :: "nat \<Rightarrow> ('a + 'b) llist \<Rightarrow> ('a + 'b) llist" where
+  "ldropLeft = undefined"
 
+lemma "stutter xs \<cdot> stutter ys \<subseteq> stutter (xs \<frown> ys)"
+  apply (auto simp add: l_prod_def)
+  apply (metis lappend_inf stutter.self stutter_lappend)
+  by (metis stutter_lappend)
 
-lemma "xs' \<in> stutter xs \<Longrightarrow> zs \<in> xs' \<sha> ys \<Longrightarrow> \<exists>Z. (\<exists>zs. Z = stutter zs \<and> zs \<in> lmap \<langle>id,id\<rangle> ` (xs \<sha> ys)) \<and> lmap \<langle>id,id\<rangle> zs \<in> Z"
-proof (induct xs' rule: stutter.induct)
-  fix \<sigma>
-  assume "zs \<in> LCons (\<sigma>, \<sigma>) xs \<sha> ys" 
-
-lemma "stutter xs \<parallel> {ys} \<subseteq> (lmap \<langle>id,id\<rangle> ` (xs \<sha> ys))\<^sup>\<dagger>"
-  apply (auto simp add: shuffle_def Stutter_def)
-  apply (rename_tac xs' zs)
-  apply (rule_tac x = "stutter (lmap \<langle>id,id\<rangle> (xs' \<triangleright> traj zs \<triangleleft> ys))" in exI)
-  apply (intro conjI)
-  apply (rule_tac x = "(lmap \<langle>id,id\<rangle> ` (xs \<sha> ys))\<^sup>\<dagger>" in exI)
-  apply simp
-
-lemma "X\<^sup>\<dagger> \<parallel> Y \<subseteq> (X \<parallel> Y)\<^sup>\<dagger>"
-  apply (simp add: shuffle_def Stutter_def)
-
-lemma Stutter_shuffle [simp]: "(X \<parallel> Y)\<^sup>\<dagger> = X\<^sup>\<dagger> \<parallel> Y\<^sup>\<dagger>"
-  apply (simp add: shuffle_def Stutter_def)
-  apply safe
+lemma interleave_append:
+  "xs \<triangleright> t \<frown> t' \<triangleleft> ys = (ltake (llength (\<ll> t)) xs \<triangleright> t \<triangleleft> ltake (llength (\<rr> t)) ys) \<frown> (ldrop (llength (\<ll> t)) xs \<triangleright> t' \<triangleleft> ldrop (llength (\<rr> t)) ys)"
   sorry
 
-lemma [simp]: "X\<^sup>\<dagger> \<parallel> {LNil}\<^sup>\<dagger> = X\<^sup>\<dagger>"
-  by (metis Stutter_shuffle par.mult.right_neutral)
+lemma tshuffle_stutter: "\<Union>{lmap \<langle>id,id\<rangle> ` (xs' \<sha> ys)|xs'. xs' \<in> stutter xs} \<subseteq> (lmap \<langle>id,id\<rangle> ` (xs \<sha> ys))\<^sup>\<dagger>"
+proof -
+  {
+  fix xs' zs'
 
-lemma [simp]: "{LNil}\<^sup>\<dagger> \<parallel> X\<^sup>\<dagger> = X\<^sup>\<dagger>"
-  by (metis Stutter_shuffle par.mult.right_neutral shuffle_comm)
+  assume "xs' \<in> stutter xs" and "zs' \<in> xs' \<sha> ys"
+
+  hence "\<exists>zs\<in>xs \<sha> ys. lmap \<langle>id,id\<rangle> zs' \<in> stutter (lmap \<langle>id,id\<rangle> zs)"
+  proof (induct xs' arbitrary: zs' rule: stutter.induct)
+    case (self \<sigma> zs')
+    thus "\<exists>zs\<in>xs \<sha> ys. lmap \<langle>id,id\<rangle> zs' \<in> stutter (lmap \<langle>id,id\<rangle> zs)"
+      sorry
+  next
+    case (stutter ws vs \<sigma> zs')
+
+    have zs'_interleave: "zs' = ws \<frown> LCons (\<sigma>, \<sigma>) vs \<triangleright> traj zs' \<triangleleft> ys"
+      sorry
+
+    have "ws \<frown> vs \<triangleright> ldeleteLeft (llength ws) (traj zs') \<triangleleft> ys \<in> (ws \<frown> vs) \<sha> ys"
+      sorry
+
+    from this and stutter(2) obtain zs where "zs \<in> xs \<sha> ys"
+    and "lmap \<langle>id,id\<rangle> (ws \<frown> vs \<triangleright> ldeleteLeft (llength ws) (traj zs') \<triangleleft> ys) \<in> stutter (lmap \<langle>id,id\<rangle> zs)"
+      by blast
+
+    moreover have "lmap \<langle>id,id\<rangle> zs' \<in> stutter (lmap \<langle>id,id\<rangle> (ws \<frown> vs \<triangleright> ldeleteLeft (llength ws) (traj zs') \<triangleleft> ys))"
+      apply (subst zs'_interleave)
+      sorry
+
+    ultimately show ?case
+      by (metis (hide_lams, no_types) stutter_trans)
+  next
+    case (mumble ws \<sigma> \<sigma>' \<sigma>'' vs zs')
+
+    have zs'_interleave: "zs' = (ws \<frown> LCons (\<sigma>, \<sigma>'') vs) \<triangleright> traj zs' \<triangleleft> ys"
+      sorry
+
+    have "(ws \<frown> LCons (\<sigma>,\<sigma>') (LCons (\<sigma>',\<sigma>'') vs) \<triangleright> linsertLeft (llength ws + 1) () (traj zs') \<triangleleft> ys) \<in> (ws \<frown> LCons (\<sigma>, \<sigma>') (LCons (\<sigma>', \<sigma>'') vs)) \<sha> ys"
+      sorry
+
+    from this and mumble(2) obtain zs where "zs \<in> xs \<sha> ys"
+    and "lmap \<langle>id,id\<rangle> (ws \<frown> LCons (\<sigma>,\<sigma>') (LCons (\<sigma>',\<sigma>'') vs) \<triangleright> linsertLeft (llength ws + 1) () (traj zs') \<triangleleft> ys) \<in> stutter (lmap \<langle>id,id\<rangle> zs)"
+      by blast
+
+    moreover have "lmap \<langle>id,id\<rangle> zs' \<in> stutter (lmap \<langle>id,id\<rangle> (ws \<frown> LCons (\<sigma>,\<sigma>') (LCons (\<sigma>',\<sigma>'') vs) \<triangleright> linsertLeft (llength ws + 1) () (traj zs') \<triangleleft> ys))"
+      apply (subst zs'_interleave)
+      sorry
+ 
+    ultimately show ?case
+      by (metis (hide_lams, no_types) stutter_trans)
+  qed
+  }
+  thus ?thesis
+    by (auto simp add: Stutter_def) (metis (full_types) imageI)
+qed
+
+lemma shuffle_stutter: "X\<^sup>\<dagger> \<parallel> Y \<subseteq> (X \<parallel> Y)\<^sup>\<dagger>"
+proof -
+  have "X\<^sup>\<dagger> \<parallel> Y = \<Union>(stutter ` X) \<parallel> Y"
+    by (rule arg_cong, auto simp add: Stutter_def image_def)
+ also have "... = \<Union>{stutter xs \<parallel> Y|xs. xs \<in> X}"
+    by (subst trans[OF shuffle_comm shuffle_inf_dist], subst shuffle_comm, auto)
+  also have "... = \<Union>{\<Union>{lmap \<langle>id,id\<rangle> ` (xs' \<sha> ys) |xs' ys. xs' \<in> stutter xs \<and> ys \<in> Y}|xs. xs \<in> X}"
+    by (simp add: shuffle_def)
+  also have "... = \<Union>{\<Union>{lmap \<langle>id,id\<rangle> ` (xs' \<sha> ys) |xs'. xs' \<in> stutter xs}|xs ys. xs \<in> X \<and> ys \<in> Y}"
+    by blast
+  also have "... \<subseteq> \<Union>{(lmap \<langle>id,id\<rangle> ` (xs \<sha> ys))\<^sup>\<dagger>|xs ys. xs \<in> X \<and> ys \<in> Y}"
+    by (insert tshuffle_stutter) blast
+  also have "... = \<Union>{\<Union>zs\<in>xs \<sha> ys. stutter (lmap \<langle>id,id\<rangle> zs)|xs ys. xs \<in> X \<and> ys \<in> Y}"
+    by (auto simp add: Stutter_def)
+  also have "... = (X \<parallel> Y)\<^sup>\<dagger>"
+    by (simp add: shuffle_def Stutter_def) blast
+  finally show ?thesis .
+qed
+
+lemma Stutter_shuffle [simp]: "(X\<^sup>\<dagger> \<parallel> Y\<^sup>\<dagger>)\<^sup>\<dagger> = (X \<parallel> Y)\<^sup>\<dagger>"
+  sorry
 
 end
 
